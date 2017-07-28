7d6972bcb8cd09a260e73c35d699532bc39a9d7b
Wei Wei <weiwei.inf@gmail.com>
2017-07-28 15:12:34 +0800

    add values cmd

--- pkg/registry/chart.go
+++ pkg/registry/chart.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"github.com/caicloud/helm-registry/pkg/rest/v1"
 	"github.com/urfave/cli"
-	"strings"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"strings"
+	//hapi "k8s.io/helm/pkg/proto/hapi/chart"
+	//"k8s.io/helm/pkg/chartutil"
+	//"encoding/json"
+	"encoding/json"
 )
+
+type chartValues map[string]interface{}
 
 var ChartCommand = cli.Command{
 	Name:  "chart",
@@ -15,6 +22,7 @@ var ChartCommand = cli.Command{
 		listChartCommand,
 		pushChartCommand,
 		inspectChartCommand,
+		getChartFileCommand,
 	},
 }
 
@@ -42,7 +50,7 @@ var listChartCommand = cli.Command{
 			for _, item := range res.Items {
 				fmt.Println("\t\t", item)
 			}
-			fmt.Println("---------------------------------------")
+			fmt.Println("-----------------------------------------")
 		} else if len(parts) == 2 {
 			res, err := c.ListVersions(parts[0], parts[1], 0, 1000)
 			if err != nil {
@@ -52,7 +60,7 @@ var listChartCommand = cli.Command{
 			for _, item := range res.Items {
 				fmt.Println("\t\t", item)
 			}
-			fmt.Println("---------------------------------------")
+			fmt.Println("-----------------------------------------")
 		} else {
 			return fmt.Errorf("invalid name `%s` must be `space` or `space/chartname`", space)
 		}
@@ -119,6 +127,64 @@ var inspectChartCommand = cli.Command{
 		}
 		fmt.Println(name, "metadata")
 		fmt.Println(res.String())
+		return nil
+	},
+}
+
+var getChartFileCommand = cli.Command{
+	Name:  "values",
+	Usage: `values space/chart:version`,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "output, o",
+			Value: "yaml",
+			Usage: `-o yaml|json`,
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		if err := checkArgs(ctx, 1, exactArgs); err != nil {
+			return err
+		}
+		c, err := v1.NewClient("http://helm-registry.ke-cs.dev.qiniu.io")
+		if err != nil {
+			return err
+		}
+		name := ctx.Args().First()
+		parts := strings.Split(name, "/")
+		if len(parts) != 2 {
+			return fmt.Errorf("invalid name `%s` must be `space/chart:version`", name)
+		}
+		space := parts[0]
+		chart := parts[1]
+		parts = strings.Split(chart, ":")
+		if len(parts) != 2 {
+			return fmt.Errorf("invalid name `%s` must be `space/chart:version`", name)
+		}
+		chartName := parts[0]
+		chartVer := parts[1]
+		raw, err := c.FetchVersionValues(space, chartName, chartVer)
+		if err != nil {
+			return err
+		}
+
+		var data []byte
+		switch ctx.String("output") {
+		case "yaml":
+			values := chartValues{}
+			yaml.Unmarshal(raw, &values)
+			data, _ = yaml.Marshal(values)
+		case "json":
+			values := chartValues{}
+			json.Unmarshal(raw, &values)
+			data, _ = json.MarshalIndent(values, "", "  ")
+		default:
+			return fmt.Errorf("unsupported output format %s", ctx.String("output"))
+		}
+
+		fmt.Println("chart", name, " values")
+		fmt.Println()
+		fmt.Println(string(data))
+		fmt.Println("------------------------------")
 		return nil
 	},
 }
